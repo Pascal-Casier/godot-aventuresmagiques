cc125fd5443ac2996c248b8bb15b208225426ae9
Pascal Casier <pascal.casier@yahoo.com.br>
2023-12-24 11:08:08 -0300

    added bullet Notebook

---
+++ scripts/mechanics/bullet_1.gd
@@ -0,0 +1,15 @@
+extends Node3D
+
+const SPEED := 40.0
+@onready var mesh_instance_3d = $MeshInstance3D
+@onready var ray_cast_3d = $RayCast3D
+
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position += transform.basis * Vector3(0, 0, -SPEED) * delta
